05c0c28cb05bc856d60e2fffd72a79f8daaf1b4a
jasimbpdb2 <jasimbpdb2@gmail.com>
2025-08-09 13:57:59 +0600

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Gurux.DLMS.Android"
+include(":app")
+include(":DLMS")
+include(":UI")
